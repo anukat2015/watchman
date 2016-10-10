430b512f3e685ddfbe8c3211352bada194716eeb
kenglish-pfi <kevin.english@soteradefense.com>
2016-10-10 15:11:38 -0500

    Prepping for CUDNN5 build

---
+++ services/configure_gpu.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Reference:
+#   https://github.com/NVIDIA/nvidia-docker/wiki/Deploy-on-Amazon-EC2
+
+# Install NVIDIA drivers 361.42
+sudo apt-get install --no-install-recommends -y gcc make libc-dev
+wget -P /tmp http://us.download.nvidia.com/XFree86/Linux-x86_64/361.42/NVIDIA-Linux-x86_64-361.42.run
+sudo sh /tmp/NVIDIA-Linux-x86_64-361.42.run --silent
+
+# Install nvidia-docker and nvidia-docker-plugin
+wget -P /tmp https://github.com/NVIDIA/nvidia-docker/releases/download/v1.0.0-rc.3/nvidia-docker_1.0.0.rc.3-1_amd64.deb
+sudo dpkg -i /tmp/nvidia-docker*.deb && rm /tmp/nvidia-docker*.deb
+
+# Install cuda
+# Reference:
+#    https://developer.nvidia.com/cuda-downloads
+sudo dpkg -i cuda-repo-ubuntu1404-8-0-local_8.0.44-1_amd64.deb
+sudo apt-get update
+sudo apt-get install cuda
+
+# Install CudNN
+sudo dpkg -i libcudnn5_5.1.5-1+cuda8.0_amd64.deb
+sudo dpkg -i libcudnn5-dev_5.1.5-1+cuda8.0_amd64.deb
+
