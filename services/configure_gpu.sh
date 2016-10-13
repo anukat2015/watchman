44787013bc9b79e631edd009c9cd4b8c379c4e75
kenglish-pfi <kevin.english@soteradefense.com>
2016-10-12 19:13:35 -0500

    Minimal steps to get Amazon AMI configured for running Caffe in a docker container using GPUs

--- services/configure_gpu.sh
+++ services/configure_gpu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for TESTFOR in "cuda-repo-ubuntu1404-8-0-local_8.0.44-1_amd64.deb" "libcudnn5_5.1.5-1+cuda8.0_amd64.deb" "libcudnn5-dev_5.1.5-1+cuda8.0_amd64.deb"
+for TESTFOR in "NVIDIA-Linux-x86_64-367.57.run" "nvidia-docker_1.0.0.rc.3-1_amd64.deb" 
 do
     if [ ! -e $TESTFOR ]
     then
@@ -12,23 +12,13 @@ done
 # Reference:
 #   https://github.com/NVIDIA/nvidia-docker/wiki/Deploy-on-Amazon-EC2
 
-# Install NVIDIA drivers 361.42
-sudo apt-get install --no-install-recommends -y gcc make libc-dev
-wget -P http://us.download.nvidia.com/XFree86/Linux-x86_64/361.42/NVIDIA-Linux-x86_64-361.42.run
-sudo sh NVIDIA-Linux-x86_64-361.42.run --silent
+# Install NVIDIA drivers 367.57
+#  This is incrementally newer than the version that we get updated to below (367.48) if we follow the instructions and install 361.42
+#  Hoping this works since 367.48 has been removed from the NVidia downloads site
+sudo apt-get -q install --no-install-recommends -y gcc make libc-dev
+sudo sh NVIDIA-Linux-x86_64-367.57.run --ui=none --no-questions --accept-license
+
+shutdown -r now
 
 # Install nvidia-docker and nvidia-docker-plugin
-wget -P /tmp https://github.com/NVIDIA/nvidia-docker/releases/download/v1.0.0-rc.3/nvidia-docker_1.0.0.rc.3-1_amd64.deb
-sudo dpkg -i /tmp/nvidia-docker*.deb && rm /tmp/nvidia-docker*.deb
-
-# Install cuda
-# Reference:
-#    https://developer.nvidia.com/cuda-downloads
-sudo dpkg -i cuda-repo-ubuntu1404-8-0-local_8.0.44-1_amd64.deb
-sudo apt-get update
-sudo apt-get install cuda
-
-# Install CudNN
-sudo dpkg -i libcudnn5_5.1.5-1+cuda8.0_amd64.deb
-sudo dpkg -i libcudnn5-dev_5.1.5-1+cuda8.0_amd64.deb
-
+sudo dpkg -i nvidia-docker_1.0.0.rc.3-1_amd64.deb
