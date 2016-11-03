e09a16b20af458f6ba643c1df23db44090fbffd6
drJAGartner <dr.j.a.gartner@gmail.com>
2016-11-03 14:14:58 -0500

    Finalize event model

--- services/dr-manhattan/louvaine.py
+++ services/dr-manhattan/louvaine.py
@@ -1,4 +1,4 @@
-import community, sys, os
+import community, sys, os, requests
 import networkx as nx
 from random import sample
 sys.path.append(os.path.join(os.path.dirname(__file__), "../util"))
@@ -7,11 +7,13 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "../rorschach/util"))
 from sentiment_filters import SentimentFilter
 
 class Louvaine:
-    def __init__(self, base_url):
+    def __init__(self, base_url, ent_url, geo_url):
         self.graph = nx.Graph()
         self.nodes_detailed = {}
         stop_file = open(os.path.dirname(__file__) + '/files/' + 'stopWordList.txt', 'r')
         self.stop = set([])
+        self.ent_url = ent_url
+        self.geo_url = geo_url
         for line in stop_file:
             self.stop.add(line.strip('\n').strip('\r'))
 
@@ -37,6 +39,8 @@ class Louvaine:
             l_sample = sample(cluster['similar_post_ids'], 50)
 
         words = {}
+        places = []
+        websites = set([])
 
         #TODO: fix query type once S.L. is fixed
         for id in l_sample:
@@ -48,8 +52,20 @@ class Louvaine:
             page = lp.get_next_page()
             if page is None:
                 continue
+
             for doc in page:
-                for word in [w for w in self.sf.tokenize(doc['text'], doc['lang']) if w not in self.stop]:
+                r = requests.post(self.ent_url, data={'text':doc['text']})
+                for res in r.json():
+                    if res['tag'] != 'LOCATION':
+                        continue
+                    rg = requests.post(self.geo_url, data={'address':res['label']})
+                    for place in rg.json():
+                        places.append(place)
+                        break
+
+                for word in [w for w in self.sf.pres_tokenize(doc['text'], doc['lang']) if w not in self.stop]:
+                    if (word[:4]=='http') or word[:3]=='www':
+                        websites.add(word)
                     if word in words:
                         words[word] += 1
                     else:
@@ -58,7 +74,8 @@ class Louvaine:
 
         words = {key:value for key, value in words.iteritems() if value > 5}
 
-        return words
+
+        return (words, places, list(websites))
 
 
     def get_img_sum(self, cluster):
@@ -106,39 +123,80 @@ class Louvaine:
                     'aggregate_cluster_ids':[n],
                     'hashtags':{},
                     'keywords':{},
-                    'urls':[],
+                    'urls':set([]),
                     'image_urls':[],
+                    'location':{},
                     'importanceScore':1.0,
                     'topicMessageCount':len(clust['similar_post_ids'])}
 
+            #Expand Summary data (hashtags, keywords, images, urls, geo)
             if clust['data_type'] == 'hashtag':
                 d1[com]['hashtags'][clust['term']] = len(clust['similar_post_ids'])
             elif clust['data_type'] == 'image':
                 d1[com]['image_urls'].extend(self.get_img_sum(clust))
             elif clust['data_type'] == 'text':
-                word_sum = self.get_text_sum(clust)
+                word_sum, places, websites = self.get_text_sum(clust)
                 for k, v in word_sum.iteritems():
                     if k in d1[com]['keywords']:
                         d1[com]['keywords'][k] += v
                     else:
                         d1[com]['keywords'][k] = v
+                for site in websites:
+                    d1[com]['urls'].add(site)
+                for place in places:
+                    place_name = ''
+                    weight = 0.0
+                    if 'city' in place.keys():
+                        place_name = place['city'] + ' '
+                        weight += 1
+                    if 'state' in place.keys():
+                        place_name += place['state'] + ' '
+                        weight += .1
+                    if 'country' in place.keys():
+                        place_name += ' ' + place['country'] + ' '
+                        weight += .05
+                    if place_name in d1[com]['location']:
+                        d1[com]['location'][place_name]['weight'] += weight
+                    else:
+                        d1[com]['location'][place_name] = {
+                            "type":"inferred point",
+                            "geo_type":"point",
+                            "coords":[{
+                                "lat": place['latitude'],
+                                "lng":place['longitude']}
+                            ],
+                            "weight":weight
+                        }
+
+
+            #Make Sure Time is Correct
             if clust['start_time_ms'] < d1[com]['start_time_ms']:
                 d1[com]['start_time_ms'] = clust['start_time_ms']
             if clust['end_time_ms'] > d1[com]['end_time_ms']:
                 d1[com]['end_time_ms'] = clust['end_time_ms']
 
-
+        #Cleanup -> transform dicst to order lists, sets to lists for easy javascript comprehension
         for com in d1.keys():
-            l_tags = map(lambda x: x[0], sorted([(k, v) for k, v in d1[com]['hashtags']], key=lambda x: x[1]))
+            l_tags = map(lambda x: x[0], sorted([(k, v) for k, v in d1[com]['hashtags'].iteritems()], key=lambda x: x[1]))
             if len(l_tags) > 10:
                 d1[com]['hashtags'] = l_tags[:10]
             else:
                 d1[com]['hashtags'] = l_tags
 
-            l_terms = map(lambda x: x[0], sorted([(k, v) for k, v in d1[com]['keywords']], key=lambda x: x[1]))
+            l_terms = map(lambda x: x[0], sorted([(k, v) for k, v in d1[com]['keywords'].iteritems()], key=lambda x: x[1]))
             if len(l_terms) > 10:
                 d1[com]['keywords'] = l_terms[:10]
             else:
                 d1[com]['keywords'] = l_terms
+
+            d1[com]['urls'] = list(d1[com]['urls'])
+
+            temp = []
+            for k, v in d1[com]['location'].iteritems():
+                dt = v
+                dt['label'] = k
+                temp.append(dt)
+            d1[com]['location'] = temp
+
 
         return d1
