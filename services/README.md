ad1e4169148b8b2e4cf2219f920e7d3cde40d9ad
Luke Wendling <luke@lukewendling.com>
2016-11-04 14:43:47 -0500

    add 'host' arg to event creation script

--- services/README.md
+++ services/README.md
@@ -12,4 +12,6 @@ docker build -f Dockerfile-py3 # ... same as above
 
 # for Dr-Manhattan
 docker build -f Dockerfile-dr-manhattan -t sotera/dr-manhattan:1 --build-arg SERVICE=dr-manhattan --build-arg MAIN=create_events_main.py .
+
+docker run --rm sotera/dr-manhattan:2 http://172.17.0.1:3000 <start_time_ms> <end_time_ms>
 ```
--- services/dr-manhattan/create_events_main.py
+++ services/dr-manhattan/create_events_main.py
@@ -3,16 +3,18 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "../util"))
 from loopy import Loopy
 from louvaine import Louvaine
 
-def create_events(ts_start, ts_end):
+def create_events(host, ts_start, ts_end):
+    if host[-1] != '/': host += '/'
+    api_path = host + 'api/'
     query_params = [{
         "query_type": "between",
         "property_name": "end_time_ms",
         "query_value": [ts_start, ts_end]
     }]
-    com = Louvaine('http://watchman:3003/api/',
-               'http://watchman:3003/api/extract/entities',
-               'http://watchman:3003/api/geocoder/forward-geo')
-    lp_n = Loopy('http://watchman:3003/api/aggregateClusters', query_params)
+    com = Louvaine(api_path,
+       '{}extract/entities'.format(api_path),
+       '{}geocoder/forward-geo'.format(api_path))
+    lp_n = Loopy('{}aggregateClusters'.format(api_path), query_params)
     while True:
         page = lp_n.get_next_page()
         if page is None:
@@ -20,7 +22,7 @@ def create_events(ts_start, ts_end):
         for doc in page:
             com.add_node(doc)
 
-    lp_e = Loopy('watchman:3003/api/clusterLinks', query_params)
+    lp_e = Loopy('{}clusterLinks'.format(api_path), query_params)
     while True:
         page = lp_e.get_next_page()
         if page is None:
@@ -34,8 +36,9 @@ def create_events(ts_start, ts_end):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("start_time", type=int,  help="Milisecond timestamp for query start")
-    parser.add_argument("end_time", type=int, help="Milisecond timestamp for query end")
+    parser.add_argument("host", help="protocol + host - Ex. http://localhost:3000 or http://watchman:3003")
+    parser.add_argument("start_time", type=int, help="Milisecond timestamp for query start")
+    parser.add_argument("end_time", type=int,help="Milisecond timestamp for query end")
     args = parser.parse_args()
 
-    create_events(args.start_time, args.end_time)
+    create_events(args.host, args.start_time, args.end_time)
