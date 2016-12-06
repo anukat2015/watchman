248e34e9f5899009781408eb277ccb1c47724808
drJAGartner <dr.j.a.gartner@gmail.com>
2016-12-06 12:40:44 -0600

    First pass at Likelihood Filter

--- services/util/date_utils.py
+++ services/util/date_utils.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 def ms_from_dt(dt):
-    return (dt - datetime.utcfromtimestamp(0)).total_seconds() * 1000
+    return int((dt - datetime.utcfromtimestamp(0)).total_seconds()) * 1000
 
 def ms_from_string(str_dt, form=1):
     dt =  str_to_dt(str_dt, form)
@@ -14,11 +14,10 @@ def dt_from_ms(ms):
 
 def str_to_dt(str_dt, form=1):
     if form==1: #ex: 2014-08-10T01:59:31.979Z - GNIP Raw data, mongo SMP "created"
-        return datetime.strptime(str_dt[:-5], "%Y-%m-%dT%H:%M:%S.000Z")
+        return datetime.strptime(str_dt[:-5], "%Y-%m-%dT%H:%M:%S")
     else:
         print "Unrecongnized format"
         return None
 
 def ms_time_interval(dt1, dt2):
-    return (dt1 - dt2).total_seconds() * 1000
-
+    return int((dt1 - dt2).total_seconds()) * 1000
