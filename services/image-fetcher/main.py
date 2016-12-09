86e555e0aa2dcdbf235096115f9e3129a3cdb8c7
Luke Wendling <luke@lukewendling.com>
2016-12-09 13:13:16 -0600

    service template

--- services/image-fetcher/main.py
+++ services/image-fetcher/main.py
@@ -2,12 +2,12 @@
 import sys, os, json
 sys.path.append(os.path.join(os.path.dirname(__file__), '../util'))
 from redis_dispatcher import Dispatcher
-from image_fetcher import fetch_image
+from threat_detector import detect
 
 
 def validate_job(job):
-    if 'urls' not in job:
-        return 'Missing "urls" required field'
+    if 'archive_url' not in job:
+        return 'Missing "archive_url" required field'
     return None
 
 def process_message(key, job):
@@ -23,41 +23,16 @@ def process_message(key, job):
         job['state'] = 'error'
         return
 
-    # local file path
-    image_path = None
-    # parsed, orig image url from page url
-    image_url = None
-
-    job_urls = job['urls'].split(',')
-
-    for url in job_urls:
-        if not url: continue
-
-        # /downloads should be in shared volume
-        image_info = fetch_image(url, download_path='/downloads/image-fetcher/')
-        if image_info:
-            image_path = image_info['image_path']
-            image_url = image_info['image_url']
-
-        # once found, break loop
-        if image_path:
-            break
-
-    if not image_path:
-        job['state'] = 'processed'
-        job['data'] = []
-        return
-
-    data = {
-        'path': image_path,
-        'url': image_url
-    }
-    job['data'] = json.dumps(data)
+    ###
+    # output = detect(job["archive_url"])
+    ###
+    
+    job['data'] = json.dumps(output)
     job['state'] = 'processed'
 
 
 if __name__ == '__main__':
     dispatcher = Dispatcher(redis_host='redis',
                             process_func=process_message,
-                            channels=['genie:fetch_image'])
+                            channels=['genie:threat-detection'])
     dispatcher.start()
